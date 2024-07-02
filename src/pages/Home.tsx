3e33d4bb62e298f48c0d46d51b2221119a99cbe1
Shota <shalania1263@gmail.com>
2024-07-02 17:43:47 +0400

    fixed code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { selectAnimeData } from "../redux/reducer/anime/slice";
-import { fetchAnime, fetchTopAnime } from "../redux/reducer/anime/reducer";
+
+import { fetchTopAnime } from "../redux/reducer/anime/reducer";
 import { CardList } from "../component/CardList";
 import { LoadedCardList } from "../component/Loader";
 import { AppDispatch } from "../redux/store/store";
@@ -17,12 +17,9 @@ import {
   selectTopMangaError,
   selectTopMangaLoading,
 } from "../redux/reducer/manga/topManga";
-import { fetchManga, fetchTopManga } from "../redux/reducer/manga/mangaReducer";
-import { selectMangaData } from "../redux/reducer/manga/slice";
+import { fetchTopManga } from "../redux/reducer/manga/mangaReducer";
 
 export const Home = () => {
-  const animeData = useSelector(selectAnimeData);
-  const mangaData = useSelector(selectMangaData);
   const topAnimeData = useSelector(selectTopAnimeData);
   const topAnimeLoading = useSelector(selectTopAnimeLoading);
   const topAnimeError = useSelector(selectTopAnimeError);
@@ -32,8 +29,6 @@ export const Home = () => {
   const dispatch: AppDispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchAnime({ page: 5, limit: 5 }));
-    dispatch(fetchManga({ page: 5, limit: 5 }));
     dispatch(fetchTopManga());
     dispatch(fetchTopAnime());
   }, [dispatch]);
@@ -46,25 +41,13 @@ export const Home = () => {
       <div className="flex flex-col justify-center items-center gap-10">
         <div className="flex flex-col justify-start items-start gap-3">
           <Link to="/anime">
-            <h1 className="text-2xl text-white cursor-pointer">Anime</h1>
-          </Link>
-          <CardList data={animeData} type="anime" />
-        </div>
-        <div className="flex flex-col justify-start items-start gap-3">
-          <Link to="/top-anime">
-            <h1 className="text-2xl text-white cursor-pointer">Top Anime</h1>
+            <h1 className="text-2xl text-white">Anime</h1>
           </Link>
           <CardList data={topAnimeData.slice(0, 5)} type="anime" />
         </div>
         <div className="flex flex-col justify-start items-start gap-3">
           <Link to="/manga">
-            <h1 className="text-2xl text-white cursor-pointer">Manga</h1>
-          </Link>
-          <CardList data={mangaData} type="manga" />
-        </div>
-        <div className="flex flex-col justify-start items-start gap-3">
-          <Link to="/top-manga">
-            <h1 className="text-2xl text-white cursor-pointer">Top Manga</h1>
+            <h1 className="text-2xl text-white">Manga</h1>
           </Link>
           <CardList data={topMangaData.slice(0, 5)} type="manga" />
         </div>
